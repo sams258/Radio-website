573829a32ca2aafe98991a0788ae3c62e1506328
Sams258 <samsse46@gmail.com>
2025-12-30 20:28:55 +0100

    updated intro

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -4,7 +4,7 @@ import "@/assets/styles/introduction.css";
 const Introduction = () => {
   return (
     <div className="introduction">
-      <h2>Welcome to LBI Group</h2>
+      <h2>Welcome to LBI Group🎄</h2>
       <p>Your Gateway to the Best Arabic and International Music</p>
       <p>Since 2011, LBI Group has been connecting listeners around the world with a diverse range of stations. From nostalgic classics on LBI Zaman to the latest hits on LBI Hits, our stations offer something for everyone.</p>
     </div>
